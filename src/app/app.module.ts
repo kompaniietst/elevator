7dc80cd82ba4514347aab7843c91c26a0a2f924e
kompaniietst <kompaniets.t@gmail.com>
2022-01-13 00:51:04 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FloorComponent } from './components/floor/floor.component';
+import { ElevatorComponent } from './components/elevator/elevator.component';
+import { BuildingComponent } from './components/building/building.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FloorComponent,
+    ElevatorComponent,
+    BuildingComponent
   ],
   imports: [
     BrowserModule,
